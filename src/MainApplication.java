6fdeb30e889af78673597c27ef8325d35368e62e
starlion10 <arron.vuong@hotmail.com>
2015-09-16 10:42:38 -0400

    new classes

---
+++ src/MainApplication.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+
+
+/*********
+ * 
+ * @author 
+ *
+ */
+
+public class MainApplication {
+
+    public static void main ( String [] arguments) throws IOException
+    {
+        System.out.println("Hello! Please input the file you wish to compress! :");
+    }
+	
+}
---
+++ src/ReadFile.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.BufferedReader;
+/*****
+ * 
+ * 
+ * @author 
+ *
+ */
+public class ReadFile {
+	
+
+}

860f86714fdfe054fd0fe175c2b917a5c75b597c
Philippe Le <philippele_1993@hotmail.com>
2015-09-28 18:31:41 -0400

    dsdsd

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -13,6 +13,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.nio.charset.Charset;
+import java.util.Collections;
 import java.util.Hashtable;
 
 /*********
--- src/NodeEncoder.java
+++ src/NodeEncoder.java
@@ -1,11 +1,20 @@
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.BitSet;
+import java.util.Hashtable;
 
 public class NodeEncoder{
 	
 	private Noeud noeudRacine;
 	private ArrayList<Character> texteAEncoderList;
 	private String encodeStringTemp="";
-	private String encodeString="";
+	private String encodeString = "";
+	private Byte[] byteArray;
+	private String decodingString = "";
+	private String stringEncodedData;
+	
 	
 	public NodeEncoder(Noeud noeud,ArrayList<Character> texteAEncoderList){
 		this.noeudRacine = noeud;
@@ -23,9 +32,28 @@ public class NodeEncoder{
 			//System.out.println(car);
 			encodeStringTemp = "";
 			searchNodes(car.charValue(),noeudRacine);
+			System.out.println(encodeStringTemp);
+			if(decodingString.indexOf(car.charValue()) == -1){
+				decodingString += car.charValue()+encodeStringTemp + ",";
+			}
 			encodeString += encodeStringTemp;
 				
 		}
+		System.out.println(decodingString);
+		//byteArray = encodeString.getBytes(Charset.forName("UTF-8"));
+		
+		short a = Short.parseShort(encodeString, 2);
+		ByteBuffer bytes = ByteBuffer.allocate(2).putShort(a);
+
+		byte[] array = bytes.array();
+		
+		for(int i=0; i< array.length;i++){
+			System.out.println("SIMON__"+array[i]);
+		}
+		for (byte b : array) {
+		    System.out.println(Integer.toBinaryString(b & 255 | 256).substring(1));
+		}
+		
 		System.out.println(encodeString);
 		
 		return null;
@@ -59,7 +87,6 @@ public class NodeEncoder{
 	    }
 	    
 	    return result;
-
 	}
 	
 	

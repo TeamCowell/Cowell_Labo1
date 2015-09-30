558778fd5bc6b3a5869ab94c07068a627e3bf3bb
sgirardbock <ak34130@etsmtl.net>
2015-09-30 02:32:20 -0400

    message translation

--- src/Decompresseur.java
+++ src/Decompresseur.java
@@ -12,18 +12,23 @@ import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.BitSet;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Decompresseur {
 	
 	private String path;
+	
 	
 	public Decompresseur(String path){
 		this.path = path;
 	}
 	
 	public void readFile() throws IOException{
+		LinkedHashMap<String,String> map = null;
+		String message = null;
 		String line;
 		boolean readBitset = false;
 	    InputStream fis = new FileInputStream(this.path);
@@ -35,6 +40,8 @@ public class Decompresseur {
 	    	
 	    	if(k==0){
 	    		System.out.println(line);
+	    		//ici on mets le code pour legende a map
+	    		map = legendeToMap(line);
 	    		
 	    	}
 	    	if(k==1){
@@ -47,7 +54,8 @@ public class Decompresseur {
         		line = line.substring(1, line.length()-1);
         		System.out.println(line);
 				try {
-					System.out.println(decodeMessage(this.path,line,bitSetSize));
+					message = decodeMessage(this.path,line,bitSetSize);
+					System.out.println("message: "+message);
 				} catch (ClassNotFoundException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -73,9 +81,43 @@ public class Decompresseur {
 	        }*/
 
 	    }
-		
+		System.out.println(translateMessage(map, message));
 		fis.close();
 	}
+	
+	private static LinkedHashMap<String,String> legendeToMap(String l){
+		l = l.substring(0, l.length()-3);
+		List<String> myList = new ArrayList<String>(Arrays.asList(l.split(",")));
+		myList.set(0, myList.get(0).substring(myList.get(0).length()-1, myList.get(0).length())) ;
+        System.out.println(myList);
+        LinkedHashMap<String,String> map = new LinkedHashMap<String,String>();
+        for(int i=0; i<myList.size(); i=i+2){
+//        	map.put(key, value)
+        	map.put(myList.get(i+1), myList.get(i));
+        	System.out.println("\""+myList.get(i) + "\" " + myList.get(i+1));
+        }
+        return map;
+	}
+	private static String translateMessage(LinkedHashMap<String,String> map, String message){
+		
+		StringBuilder resultat  = new StringBuilder();
+		while(message.length() > 0){
+			for (Map.Entry<String, String> entry : map.entrySet()) {
+				String key = entry.getKey();
+				String value = entry.getValue();
+				// now work with key and value...
+				System.out.println("going thru map with key " + key);
+				if(message.startsWith(key)){
+					resultat.append(value);
+					message = message.substring(key.length());
+				}
+
+			}
+		}
+		return resultat.toString();
+	}
+	
+	
 	
 	private static String decodeMessage(String path, String l,int size) throws FileNotFoundException, IOException, ClassNotFoundException {
         //try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(path))) {
@@ -95,7 +137,7 @@ public class Decompresseur {
             	}
             }
             System.out.println(myList.size());
-            System.out.println(bitSetString);
+            System.out.println("true mess: "+bitSetString);
             final StringBuilder stringBuilder = new StringBuilder();
             
             //System.out.println(Integer.toBinaryString(toByteArray(bitSet)[0]));
@@ -113,7 +155,7 @@ public class Decompresseur {
                }
                 stringBuilder.append(temp.valeur);
             }*/
-            return stringBuilder.toString();
+            return bitSetString;
        // }
     }
 
